55fdf86fbe1a3e6cc3a0061075447d6f8f8cebd9
Claudius26 <osomzed@gmail.com>
2025-06-16 16:06:41 +0100

    secopnd commit

--- src/main/java/com/expense/data/model/Expense.java
+++ src/main/java/com/expense/data/model/Expense.java
@@ -1,4 +1,18 @@
 package com.expense.data.model;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document
+@Data
 public class Expense {
+    @Id
+    private String id;
+    private String description;
+    private double amount;
+    private LocalDateTime date;
+
 }
